a4fb253b9f9649a9f3faab062cf128ac833d7a48
Ioannis Drossas <idrossas@csd.auth.gr>
2022-07-22 19:56:19 +0300

    Finished NRZI,Bipolar-AMI and Pseudoternary

--- main.py
+++ main.py
@@ -24,3 +24,52 @@ def calculate_nrzl(user_input):
         else:
             output.append(-1)
     return output
+
+
+def calculate_nrzi(user_input):
+    output = [1]
+    index = 1
+    for k in user_input:
+        if k == '0':
+            output.append(output[index-1])
+            index += 1
+        else:
+            output.append(-output[index-1])
+            index += 1
+    return output
+
+
+def calculate_bipolar_ami(user_input):
+    output = []
+    found_first_one = False
+    number_of_ones = 0
+    for k in user_input:
+        if k == '0':
+            output.append(0)
+        else:
+            if not found_first_one:
+                output.append(1)
+                found_first_one = True
+                number_of_ones += 1
+            else:
+                dummy = (-1)**number_of_ones
+                output.append(dummy)
+    return output
+
+
+def calculate_pseudoternary(user_input):
+    output = []
+    found_first_zero = False
+    number_of_zeros = 0
+    for k in user_input:
+        if k == '1':
+            output.append(0)
+        else:
+            if not found_first_zero:
+                output.append(1)
+                found_first_zero = True
+                number_of_zeros += 1
+            else:
+                dummy = (-1)**number_of_zeros
+                output.append(dummy)
+    return output
