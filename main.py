99a81e882da1016afad1768555b8075e79e6dc85
Ioannis Drossas <idrossas@csd.auth.gr>
2022-07-23 17:49:22 +0300

    Added Manchester Encoding and Differential Manchester + finished visualisation for NRZ encoding

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+
+
 def is_empty_input(user_input):
     return len(user_input) == 0
 
@@ -12,6 +15,7 @@ def is_binary_data(user_input):
 def calculate_nrz(user_input):
     output = []
     for k in user_input:
+        output.append(int(k))
         output.append(int(k))
     return output
 
@@ -73,3 +77,64 @@ def calculate_pseudoternary(user_input):
                 dummy = (-1)**number_of_zeros
                 output.append(dummy)
     return output
+
+
+def calculate_manchester_encoding(user_input):
+    output = []
+    for k in user_input:
+        if k == '1':
+            output.append(0)
+            output.append(0)
+            output.append(1)
+            output.append(1)
+        else:
+            output.append(1)
+            output.append(1)
+            output.append(0)
+            output.append(0)
+    return output
+
+
+def calculate_differential_manchester(user_input):
+    output = [0]
+    index = 1
+    for k in user_input:
+        if k == '1':
+            output.append(output[index-1])
+            index += 1
+            if output[index] == 0:
+                output.append(1)
+            else:
+                output.append(0)
+            index += 1
+        else:
+            if output[index-1] == 0:
+                output.append(1)
+            else:
+                output.append(0)
+            index += 1
+            if output[index] == 0:
+                output.append(1)
+            else:
+                output.append(0)
+            index += 1
+    return output
+
+
+def visualise_nrz(output, input):
+    x_axis = [0, 1]
+    index = 1
+    for k in range(1, len(input)):
+        if input[k-1] != input[k]:
+            x_axis.append(x_axis[len(x_axis)-1])
+            index += 1
+            x_axis.append(index)
+        else:
+            x_axis.append(index)
+            index += 1
+            x_axis.append(index)
+
+    plt.plot(x_axis,output)
+    plt.ylim(-1, 2)
+    plt.xlim(0, index)
+    plt.show()
