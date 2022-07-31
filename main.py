23ad845f3a58f015c396594f7586edfb96872ccb
Ioannis Drossas <idrossas@csd.auth.gr>
2022-07-31 16:03:44 +0300

    Finished all visualisations(except Differential Manchester) Calculation of Differential Manchester needs fixing as well

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy
 
 
 def is_empty_input(user_input):
@@ -55,14 +56,18 @@ def calculate_bipolar_ami(user_input):
     for k in user_input:
         if k == '0':
             output.append(0)
+            output.append(0)
         else:
             if not found_first_one:
+                output.append(1)
                 output.append(1)
                 found_first_one = True
                 number_of_ones += 1
             else:
-                dummy = (-1)**number_of_ones
+                dummy = (-1) ** number_of_ones
                 output.append(dummy)
+                output.append(dummy)
+                number_of_ones += 1
     return output
 
 
@@ -73,14 +78,18 @@ def calculate_pseudoternary(user_input):
     for k in user_input:
         if k == '1':
             output.append(0)
+            output.append(0)
         else:
             if not found_first_zero:
+                output.append(1)
                 output.append(1)
                 found_first_zero = True
                 number_of_zeros += 1
             else:
-                dummy = (-1)**number_of_zeros
+                dummy = (-1) ** number_of_zeros
                 output.append(dummy)
+                output.append(dummy)
+                number_of_zeros += 1
     return output
 
 
@@ -102,27 +111,35 @@ def calculate_manchester_encoding(user_input):
 
 def calculate_differential_manchester(user_input):
     output = [0]
-    index = 1
+    index = 0
     for k in user_input:
         if k == '1':
-            output.append(output[index-1])
-            index += 1
+            output.append(output[index])
+            output.append(output[index])
+            index += 2
             if output[index] == 0:
+                output.append(1)
                 output.append(1)
             else:
                 output.append(0)
-            index += 1
+                output.append(0)
+            index += 2
         else:
-            if output[index-1] == 0:
-                output.append(1)
-            else:
-                output.append(0)
-            index += 1
             if output[index] == 0:
                 output.append(1)
+                output.append(1)
             else:
                 output.append(0)
-            index += 1
+                output.append(0)
+            index += 2
+            if output[index] == 0:
+                output.append(1)
+                output.append(1)
+            else:
+                output.append(0)
+                output.append(0)
+            index += 2
+    output.pop(0)
     return output
 
 
@@ -130,15 +147,15 @@ def visualise_nrz(output, input):
     x_axis = [0, 1]
     index = 1
     for k in range(1, len(input)):
-        if input[k-1] != input[k]:
-            x_axis.append(x_axis[len(x_axis)-1])
+        if input[k - 1] != input[k]:
+            x_axis.append(x_axis[len(x_axis) - 1])
             index += 1
             x_axis.append(index)
         else:
             x_axis.append(index)
             index += 1
             x_axis.append(index)
-    plt.plot(x_axis,output)
+    plt.plot(x_axis, output)
     plt.ylim(-1, 2)
     plt.xlim(0, index)
     plt.show()
@@ -178,4 +195,64 @@ def visualise_nrzi(output, input):
     plt.ylim(-2, 2)
     plt.xlim(0, index)
     plt.show()
+
+
+def visualise_bipolar_ami(output, input):
+    x_axis = [0, 1]
+    index = 1
+    for k in range(1, len(input)):
+        if input[k] == '0' and input[k - 1] == '0':
+            index += 1
+            x_axis.append(index)
+            index += 1
+            x_axis.append(index)
+        else:
+            x_axis.append(index)
+            index += 1
+            x_axis.append(index)
+    plt.plot(x_axis, output)
+    plt.ylim(-2, 2)
+    plt.xlim(0, index)
+    plt.show()
+
+
+def visualise_pseudoternary(output, input):
+    x_axis = [0, 1]
+    index = 1
+    for k in range(1, len(input)):
+        if input[k] == '1' and input[k - 1] == '1':
+            index += 1
+            x_axis.append(index)
+            index += 1
+            x_axis.append(index)
+        else:
+            x_axis.append(index)
+            index += 1
+            x_axis.append(index)
+    plt.plot(x_axis, output)
+    plt.ylim(-2, 2)
+    plt.xlim(0, index)
+    plt.show()
+
+
+def visualise_manchester_encoding(output, input):
+    x_axis = [0, 1, 1, 2]
+    index = 1
+    for k in range(1, len(input)):
+        x_axis.append(index)
+        index += 1
+        x_axis.append(index)
+        x_axis.append(index)
+        index += 1
+        x_axis.append(index)
+    plt.plot(x_axis, output)
+    plt.ylim(-0.5, 1.5)
+    plt.xticks(numpy.arange(0, 2*len(input)+0.05, step=1), rotation=90)
+    plt.xlim(0, 2 * len(input) - 1)
+    plt.show()
+
+
+a = "0100110100"
+b = calculate_differential_manchester(a)
+print(b)
 
